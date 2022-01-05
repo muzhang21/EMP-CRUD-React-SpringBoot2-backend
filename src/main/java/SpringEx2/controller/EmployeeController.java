3eacc8b36b7aae0a69954f640c801c0014b7a70c
Mu Zhang <zhang106@sheridancollege.ca>
2022-01-05 14:00:48 -0500

    changed CrossOrigin with heroku

--- src/main/java/SpringEx2/controller/EmployeeController.java
+++ src/main/java/SpringEx2/controller/EmployeeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import SpringEx2.exception.ResourceNotFoundException;
 import SpringEx2.model.Employee;
 import SpringEx2.repository.EmployeeRepository;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://emp-crud-react-frontend.herokuapp.com/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
